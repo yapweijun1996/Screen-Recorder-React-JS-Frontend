ebb71007a2f450b01a2228dd72fbfc12f812ed01
yapweijun1996 <yapweijun1996@gmail.com>
2025-12-12 00:00:02 +0800

    New

--- 0_backup/v2/0_development/vite.config.ts
+++ 0_backup/v2/0_development/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     root: '.',
+    base: './',
     server: {
       port: 3000,
       host: '0.0.0.0',
