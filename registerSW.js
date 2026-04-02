d0c3c8e82b54d73f93dd8aae1e00d0281e54719a
YAP WEI JUN <yapweijun1996@gmail.com>
2026-04-02 13:49:18 +0800

    New

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Screen-Recorder-React-JS-Frontend/sw.js', { scope: '/Screen-Recorder-React-JS-Frontend/' })})}
